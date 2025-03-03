ebce63b25d6f57a4145750c3073b13a902e3c255
ThuGhacK ThuGz <thughack@hotmail.com>
2025-03-03 20:24:22 +0100

    Add Babel configuration for JavaScript transpilation

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                browsers: '> 0.25%, not dead'
+            },
+            useBuiltIns: 'usage',
+            corejs: 3
+        }],
+        '@babel/preset-react'
+    ],
+    plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-class-properties'
+    ]
+};
