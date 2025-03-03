97c31de2c878c8503ace0ab74064033d810e0b5a
ThuGhacK ThuGz <thughack@hotmail.com>
2025-03-03 20:34:01 +0100

    Remove Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', {
-            targets: {
-                browsers: '> 0.25%, not dead'
-            },
-            useBuiltIns: 'usage',
-            corejs: 3
-        }],
-        '@babel/preset-react'
-    ],
-    plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-class-properties'
-    ]
-};
+# Removed
